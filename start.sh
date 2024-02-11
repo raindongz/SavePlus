394d9eaaf239b587e1cdfd04b5c1b9a5c3a4c694
RyanZhong <ryandzhong@gmail.com>
2024-02-10 22:05:20 -0500

    introduced dockerfile

---
+++ start.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+set -e
+
+echo "run db migration"
+source /app/app.env
+echo $DB_SOURCE
+/app/migrate -path /app/migration -database "$DB_SOURCE" -verbose up
+echo "start the app"
+# pass all parameters to the script and run it
+exec "$@"
